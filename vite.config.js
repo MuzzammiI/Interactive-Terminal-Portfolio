5b90566ab1d83b40a3e9093c0c23de8860e0c7db
muzzu <www.mdmozammil@gmail.com>
2025-07-25 18:28:19 +0530

    Add Repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/terminal-portfolio/',
+  base: '/Interactive-Terminal-Portfolio/',
+   build: {
+    outDir: 'dist', // This is the default, but good to confirm
+  },
 })
